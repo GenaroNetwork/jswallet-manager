db54e7b2cdebb32432e0cdec0224a626bd372bad
lishi <lishi1608@gmail.com>
2018-07-02 20:35:59 +0800

    update version
    Signed-off-by: lishi <lishi1608@gmail.com>

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function scanFolder (homePath) {
     throw new Error(errors.NOT_A_DIR)
   }
   fs.readdirSync(homePath).forEach(file => {
-    if(!file.startsWith('UTC--')) return // ethereumjs-wallet default name: wallet.getV3Filename()
+    if (!file.startsWith('UTC--')) return // ethereumjs-wallet default name: wallet.getV3Filename()
     const fullPath = path.join(homePath, file)
     try {
       let bFile = fs.lstatSync(fullPath).isFile()
